2564ce0a70109b2edf63268cdf7a99150964e8ba
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2021-05-24 21:32:01 +0300

    Add files via upload

---
+++ hipotenus.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int kenar1,kenar2;
+        double hipotenüs;
+
+	    Scanner input = new Scanner(System.in);
+        System.out.print("ilk kenarı girin:");
+        kenar1 = input.nextInt();
+
+        System.out.print("ikinci kenarı girin:");
+        kenar2 = input.nextInt();
+
+        hipotenüs = Math.sqrt(Math.pow(kenar1,2) + Math.pow(kenar2,2));
+        System.out.print("hipotenüs uzunlugu:" + hipotenüs);
+
+
+
+
+
+    }
+}
