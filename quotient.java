b65c40b8e220c3e78d6c2c99b21dedf4a3bfd2f0
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2021-06-11 17:27:43 +0300

    Update quotient.java

--- quotient.java
+++ quotient.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int sum = 0;
-        float average;
+        float average = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a number:");
         int number = input.nextInt();
         int flag = 0;
-        for(int i = 0; i <= number; i++){
+        for(int i = 1; i < number; i++){
             if(i % 3 == 0 && i % 4 == 0){
-                sum += i;
                 flag++;
+                sum += i;
+                average = sum / flag;
             }
 
         }
-        average = sum / flag;
         System.out.println("Sum is:" + sum);
         System.out.println("Average is:" + average);
 
