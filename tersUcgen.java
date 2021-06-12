a96f5c58ec978ba9f14baa6e9c40a8751fbfdbd2
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2021-06-12 21:33:47 +0300

    Create tersUcgen.java

---
+++ tersUcgen.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = n; i >= 0 ; i--) {
+            for (int j = (n - i); j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+}
