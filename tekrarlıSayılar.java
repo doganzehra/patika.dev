3f2785a95a917bf87be70e9241d57bf4d0bc7e56
Zehra Doğan <77925097+doganzehra@users.noreply.github.com>
2021-06-14 19:56:01 +0300

    Create tekrarlıSayılar.java

---
+++ tekrarlıSayılar.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Main {
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {58,45,2,6,7,98,98,101,1,2,5,6,7,10};
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if ((i != j) && (list[i] == list[j])) {
+                    if (!isFind(duplicate, list[i])) {
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int value : duplicate){
+            if (value != 0){
+                System.out.println(value);
+            }
+        }
+    }
+}
